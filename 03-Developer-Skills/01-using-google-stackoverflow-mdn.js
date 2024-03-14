e1780e12007647b6b3b2dff661c021b568b24ec0
nguyenthuydung <nthuydung.1603@gmail.com>
2024-03-14 15:15:37 +0700

    Developer Skills & Editor Setup

---
+++ 03-Developer-Skills/01-using-google-stackoverflow-mdn.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temperature amplitude? Answer: difference between highest and lowest temperature
+// - How to compute max and min temperature?
+// - What's a sensor error? And what to do?
+
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temperature array
+// - Find minimum value in temperature array
+// - Subtract min from max (amplitude) and return it
+
+const calcTemAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTemAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge 2 arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+
+const calcTemAmplitudeNew = function (t1,t2) {
+  const temps = t1.concat(t2);
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTemAmplitudeNew([3,5,1],[9,0,5]);
+console.log(amplitudeNew);
---
+++ 03-Developer-Skills/script.js
@@ -0,0 +1,15 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = 23;
+
+const calcAge = birthYear => 2037 - birthYear;
+console.log(calcAge(1991));
+
+// Framework problem -solving
+// + Ask the right questions to get a clear picture of the problem
+// + Divide and conquer: break into smaller  sub - problem
+// + First do by yourself, then don't be afraid to research
+// + Write pseudo-code
+
+
